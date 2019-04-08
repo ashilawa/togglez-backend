cecf6c915e1e1dcb9dc996333fd78034128218f2
Afshar Ahmed <AfsharA@citiustech.com>
2019-04-08 11:35:46 +0530

    Added whitelisted URLs for CORS mapping

--- src/main/java/com/citiustech/TogglezApplication.java
+++ src/main/java/com/citiustech/TogglezApplication.java
@@ -2,6 +2,11 @@ package com.citiustech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.citiustech.config.CustomWebMvcConfigurer;
 
 @SpringBootApplication
 public class TogglezApplication {
@@ -9,4 +14,15 @@ public class TogglezApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TogglezApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new CustomWebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+                //.allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
 }
